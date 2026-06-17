223408e1db44aa02adf0b4fad55db2fbfdf551be
HEMAMALINI2 <jaganmalini2@gmail.com>
2026-06-17 19:48:50 +0530

    Remove Duplicates

---
+++ Problem145.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+
+public class Problem145{
+    public static void main(String[] args) {
+        int[] arr={1, 2, 3, 1, 2, 3, 4};
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            if(!list.contains(arr[i])){
+                list.add(arr[i]);
+            }
+        }
+
+        for(int i=0;i<list.size();i++){
+            System.out.print(list.get(i)+" ");
+        }
+    }
+}
