39d205b9176a291e773246321a967bd8e6d2635a
HEMAMALINI2 <jaganmalini2@gmail.com>
2026-06-17 19:36:36 +0530

    Second Largest Element

---
+++ Problem147.java
@@ -0,0 +1,18 @@
+public class Problem147{
+    public static void main(String[] args) {
+        int[] arr = {45, 25, 69, 40};
+        int first = arr[0];
+        int second = 0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>first){
+                second=first;
+                first=arr[i];
+            }
+            else if(arr[i]>second && second!=first){
+                second=arr[i];
+            }
+        }
+
+        System.out.println(second);
+    }
+}
