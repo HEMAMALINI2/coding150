184902d1a4bc5819e872312aae513bf230c159bb
HEMAMALINI2 <jaganmalini2@gmail.com>
2026-06-18 10:52:54 +0530

    Sum of digits upto Single digit

---
+++ Problem141.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Problem141{
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       int n = in.nextInt();
+       int ans=0;
+       while(n>0){
+        ans+=n%10;
+        n/=10;
+        if(n==0 && ans>9){
+            n=ans;
+            ans=0;
+        }
+       } 
+
+       System.out.println(ans);
+    }
+}
